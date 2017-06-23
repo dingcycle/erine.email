1d8dc91c133c212975b3f35b39f21579fef0386f
mdavranche <mdavranche@erine.email>
2017-06-23 11:46:50 +0200

    Apply for the GNU General Public License v3.0

--- modules/postfix/files/spameater.py
+++ modules/postfix/files/spameater.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
-# http://mysql-python.sourceforge.net/MySQLdb.html
+# Copyright (C) 2017 Mikael Davranche
+
+# This file is part of erine.email project. https://erine.email
+
+# erine.email is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with erine.mail.  If not, see <http://www.gnu.org/licenses/>.
+
 import MySQLdb
 
 import logging
